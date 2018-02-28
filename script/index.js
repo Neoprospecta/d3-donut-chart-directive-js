a91197e55fc59ff4c265c5e5335a5e0da1373891
Wellington Rutes <netorutes@gmail.com>
2018-02-27 21:54:31 -0300

    Created tooltips when mouse hover.

--- script/index.js
+++ script/index.js
@@ -2,6 +2,7 @@ const DEFAULT_SIZE = 260;
 var thickness = 40;
 var duration = 750;
 
+var div = d3.select(".donut-chart").append("div").attr("class", "toolTip");
 d3.selectAll(".donut-chart")
     .append('svg')
     .attr('class', 'pie')
@@ -48,24 +49,57 @@ d3.selectAll(".donut-chart")
             var data = res;
 
             // add and colour the donut slices
-            g.select('.slices')
+            var path = g.select('.slices')
                 .datum(data)
                 .selectAll('path')
                 .data(pie)
                 .enter()
                 .append('path')
+                .style("opacity", 0.9)
                 .attr('d', arc)
                 .attr('fill', (d, i) => color(i))
+                .on("mousemove", function (d) {
+                    div.transition()
+                        .duration(150)
+                        .style("opacity", 1);
+                    div.style("left", d3.event.pageX + 10 + "px");
+                    div.style("top", d3.event.pageY - 25 + "px");
+                    div.html((d.data.value) + "% <br> bar");
+                })
                 .on("mouseover", function (d) {
                     d3.select(this)
                         .style("cursor", "pointer")
-                        .style("opacity", "0.5");
+                        .style("opacity", 1);
                 })
                 .on("mouseout", function (d) {
                     d3.select(this)
                         .style("cursor", "none")
                         .style("fill", color(this._current))
-                        .style("opacity", "1");
+                        .style("opacity", 0.9);
+                    div.transition()
+                        .duration(300)
+                        .style("opacity", 0);
+                    d3.selectAll('.toolCircle').remove();
+    
+                })
+                .on('mouseenter', function (data) {
+                    console.log('data = ', data);
+                    console.log('svg = ', svg);
+                    svg.append('text')
+                        .attr('transform', 'translate(' + (width / 2) + ',' + (width / 2) + ')')
+                        .attr('class', 'toolCircle')
+                        .attr('dy', -15) // hard-coded. can adjust this to adjust text vertical alignment in tooltip
+                        .html("teste") // add text to the circle.
+                        .style('font-size', '.9em')
+                        .style('text-anchor', 'middle'); // centres text in tooltip
+    
+                    svg.append('circle')
+                        .attr('transform', 'translate(' + (width / 2) + ',' + (width / 2) + ')')
+                        .attr('class', 'toolCircle')
+                        .attr('r', radius * 0.55) // radius of tooltip circle
+                        .attr('fill', color(this._current)) // colour based on category mouse is over
+                        .style('fill-opacity', 0.35);
+    
                 })
                 .each(function (d, i) { this._current = i; });
 
@@ -95,22 +129,24 @@ d3.selectAll(".donut-chart")
                     return (midAngle(d)) < Math.PI ? 'start' : 'end';
                 });
 
-                // add lines connecting labels to slice. A polyline creates straight lines connecting several points
+            // add lines connecting labels to slice. A polyline creates straight lines connecting several points
             g.select('.lines')
                 .datum(data)
                 .selectAll('polyline')
                 .data(pie)
                 .enter()
                 .append('polyline')
-                .attr('points', function(d) {
+                .attr('points', function (d) {
                     // see label transform function for explanations of these three lines.
                     var pos = outerArc.centroid(d);
                     pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
                     return [arc.centroid(d), outerArc.centroid(d), pos]
                 });
+
         });
 
         function midAngle(d) {
             return d.startAngle + (d.endAngle - d.startAngle) / 2;
         }
+
     });
